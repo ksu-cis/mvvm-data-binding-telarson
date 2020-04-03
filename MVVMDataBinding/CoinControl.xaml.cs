956529e64f3817315fc584d434b28fd408cfdc8c
Tristan Larson <telarson@ksu.edu>
2020-04-03 11:59:03 -0500

    Works but bill controls are squished

---
+++ MVVMDataBinding/CoinControl.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CashRegister;
+
+namespace MVVMDataBinding
+{
+    /// <summary>
+    /// Interaction logic for CoinControl.xaml
+    /// </summary>
+    public partial class CoinControl : UserControl
+    {
+        public CoinControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty DenominationProperty = DependencyProperty.Register(
+            "Denomination",
+            typeof(Coins),
+            typeof(CoinControl),
+            new PropertyMetadata(Coins.Nickel)
+            );
+
+        /// <summary>
+        /// The denomination of the coin
+        /// </summary>
+        public Coins Denomination
+        {
+            get { return (Coins)GetValue(DenominationProperty); }
+            set { SetValue(DenominationProperty, value); }
+        }
+
+        /// <summary>
+        /// The DependencyProperty backing the Quantity property
+        /// </summary>
+        public static readonly DependencyProperty QuantityProperty =
+                DependencyProperty.Register(
+                    "Quantity",
+                    typeof(int),
+                    typeof(CoinControl),
+                    new FrameworkPropertyMetadata(
+                        0,
+                        FrameworkPropertyMetadataOptions.BindsTwoWayByDefault
+                    )
+                );
+
+        /// <summary>
+        /// The quantity of the coin denomination
+        /// </summary>
+        public int Quantity
+        {
+            get { return (int)GetValue(QuantityProperty); }
+            set { SetValue(QuantityProperty, value); }
+        }
+
+        /// <summary>
+        /// Increases the quantity of the bound coinage by one
+        /// </summary>
+        /// <param name="sender">The coinage quanity (as an int)</param>
+        /// <param name="args">The event args</param>
+        public void OnAddClicked(object sender, RoutedEventArgs args)
+        {
+            Quantity++;
+        }
+
+
+        /// <summary>
+        /// Decreases the quantity of the bound coinage by one
+        /// </summary>
+        /// <param name="sender">The coinage quantity (as in int)</param>
+        /// <param name="args">The event args</param>
+        public void OnRemoveClicked(object sender, RoutedEventArgs args)
+        {
+            Quantity--;
+        }
+
+
+    }
+}
